2408c577f570ea93d413429869b3b89c69d6704d
NithikK <nithik.k@gavstech.com>
2023-10-27 11:43:50 +0530

    CommitAll

--- src/App.js
+++ src/App.js
@@ -38,12 +38,7 @@ function App() {
   function fetchdata() {
     axios.get('http://localhost:9901/getAll')
       .then((response) => {
-        const filteredData = response.data.filter(item => 
-          item.userid !== null && item.userid !== '' &&
-          item.password !== null && item.password !== '' &&
-          item.emailid !== null && item.emailid !== ''
-        );
-        setUserData(filteredData);
+        setUserData(response.data);
       })
       .catch((error) => {
         console.error('Error:', error);
@@ -105,7 +100,7 @@ function App() {
             <td><button type="reset">Reset</button></td>
             <td><button type="button" onClick={modify}>Modify</button></td>
             <td><button type="delete" onClick={remove}>Remove</button></td>
-            <td><button onClick={fetchdata}>Show</button></td>
+            <td><button type="button" onClick={fetchdata}>Show</button></td>
           </tr>
         </table>
       </form>
@@ -127,86 +122,3 @@ function App() {
 }
 
 export default App;
-
-
-
-
-
-
-/*
-import './App.css';
-import { useEffect, useState } from 'react';
-import { Axios } from 'axios';
-
-
-function App() {
-    let [data, setdata] = useState([]);
-    let [showData, setshowData] = useState(false);
-    let [userData, setUserData] = useState([]);
-    const [userid, setUserId] = userState('');
-    const [password, setPassword] = useState('');
-    const [email, setEmail] = useState('');
-
-    useEffect(() => {
-        fetch("http://localhost:9901/getAll")
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            // .then((data)=>{setdata(data);});
-            .then((data) => { setdata(data); })
-            .then(data => console.log(data));
-    }, []);
-
-
-    const nowshowData = () => {
-        setshowData(true);
-    }
-
-    const updateUserId = (event) => {
-        setUserId(event.target.value);
-    }
-
-
-    const updatePassword = (event) => {
-        setPassword(event.target.value);
-    }
-
-
-    const updateEmail = (event) => {
-        setEmail(event.target.value)
-    }
-
-    const insertUser = () => {
-        axios.post('http://localhost:9901/insert' , [userid,email,password]);
-    }
-
-
-    return (
-        <div className="App">
-            <center>
-                <form onSubmit={}>
-                    <b>User ID</b><input type="text" value={userid}
-                        onChange={updateUserId} /><br />
-                    <b>Password</b><input type="password" value={password}
-                        onChange={updatePassword} /><b />
-                    <b>Email ID</b><input type="email" value={email}
-                        onChange={updateEmail} /><br />
-                    <input type="submit" value="Add" />&nbsp;&nbsp;
-                    <input type="reset" value="Reset" />
-                </form></center>
-            <button onClick={nowshowData}>Show Data</button>
-            {showData && (<ol>
-                {data.map((item) => (
-                    <li key={item.userid}>ID: {item.userid}, Password: {item.password}, Email: {item.email}</li>
-                ))}
-            </ol>)}
-
-        </div>
-    );
-}
-
-export default App;
-*/
